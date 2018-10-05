41aaa4856a5b42d694d3a29dfed8aeca1085ddb7
Kaysha Chandran <mykaysha@gmail.com>
2018-10-05 14:19:45 -0700

    add source, xml and yaml files

---
+++ src/SumInt.java
@@ -0,0 +1,13 @@
+
+public class SumInt {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		System.out.println(sumTwo(a,b));
+	}
+	
+	public static int sumTwo(int a, int b) {
+		return (a + b);
+	}
+}
